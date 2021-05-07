90dd41284be1ec24b1fd554887b74f19e480c80e
RishiRanseom99 <41407371+RishiRanseom99@users.noreply.github.com>
2021-05-07 11:22:08 +0530

    Update HttpAdmin.js

--- src/webRTC/HttpAdmin.js
+++ src/webRTC/HttpAdmin.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 export default function HttpAdmin() {
+  const [username,insertUsername] = useState("testing")
   const [localStream, setlocalStream] = useState(null);
   const [remoteStream, setremoteStream] = useState(null);
   const [peerConn, setPeerConn] = useState(null);
@@ -27,15 +28,17 @@ export default function HttpAdmin() {
 
   const handleLocalStream = (stream) => {
     const video = document.getElementById("local-video");
-    video.srcObject = stream;
+    // video.srcObject = stream;
     setlocalStream(stream);
   };
 
   const videoError = (err) => {
     console.log(err.name, err);
   };
-  const refactorResponse = (res) => {};
-
+   
+ const handleUsernameOnChange = (e)=>{
+   insertUsername(e.target.value)
+ }
   const createAndSendAnswer = () => {
     console.log("create and sending the answer");
     peerConn.createAnswer(
@@ -86,16 +89,16 @@ export default function HttpAdmin() {
 
     peerConn.addStream(localStream);
 
-    peerConn.onaddstream = (e) => {
+    peerConn.ontrack = (e) => {
       console.log("adding stream");
-      document.getElementById("remote-video").srcObject = e.stream;
-      setremoteStream(e.stream);
+      document.getElementById("remote-video").srcObject = e.streams[0];
+      setremoteStream(e.streams[0]);
     };
     peerConn.onicecandidate = (e) => {
       if (e.candidate == null) {
         if (adminData.current.candidates.length > 0) {
           axios.post(
-            "https://1fd91683bdb5.ngrok.io/adminget?email=rishi&qid=80",
+            `https://1fd91683bdb5.ngrok.io/admin?email=${username}&qid=80`,
 
             adminData.current
           );
@@ -108,15 +111,16 @@ export default function HttpAdmin() {
 
     axios
       .get(
-        "https://1fd91683bdb5.ngrok.io/adminserve?email=rishi&qid=80",
+        `https://1fd91683bdb5.ngrok.io/admin?email=${username}&qid=80`,
         { crossdomain: true }
       )
       .then((response) => {
         console.log(response.data);
-        candidateData.current.offer = response.data.offer
-        candidateData.current.candidates = response.data.candidates
-        console.log( candidateData.current.offer)
-        console.log(  candidateData.current.candidates)
+        if(response.data === "User Not Found"){
+          alert("user not available")
+        }else{
+        candidateData.current.offer = response.data?.offer
+        candidateData.current.candidates = response.data?.candidates
         peerConn.setRemoteDescription(
           new RTCSessionDescription(candidateData.current.offer)
         );
@@ -125,7 +129,7 @@ export default function HttpAdmin() {
           peerConn.addIceCandidate(candidate);
         });
         
-
+      }
         //   console.log(response.data)
 
       })
@@ -137,17 +141,21 @@ export default function HttpAdmin() {
   return (
     <div>
       <div>
+        <h1>admin side:</h1>
         <input
-          placeholder="Enter username..."
+          placeholder="Choose username: 'testing'"
           type="text"
           id="username-input"
+          value={username}
+          onChange={handleUsernameOnChange}
+
         />
         <br />
         {/* <button onClick={sendUsername}>Send</button> */}
         <button onClick={joinCall}>Start Call</button>
       </div>
       <div id="video-call-div">
-        <video src={localStream} muted id="local-video" autoPlay></video>
+        {/* <video src={localStream} muted id="local-video" autoPlay></video> */}
         <video src={remoteStream} id="remote-video" autoPlay></video>
         <div className="call-action-div">
           {/* <button onclick={muteVideo}>Mute Video</button> */}
